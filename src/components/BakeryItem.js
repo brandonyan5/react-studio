4adbcd5d8d34467cb86dc6d9599d6de0caa522c7
Brandon Yan <35352988+brandonyan5@users.noreply.github.com>
2022-11-10 18:46:57 -0500

    Update BakeryItem.js

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,28 @@
 // TODO: create a component that displays a single bakery item
+
+import React from "react";
+
+class BakeryItem extends React.Component {
+
+
+    render() {
+        const addToCart = this.props.addFunction;
+        const addToCartPrice = this.props.addFunctionPrice;
+        return (
+            <div>
+                <p>{this.props.name}</p>
+                <p>{this.props.description}</p>
+                <p>{this.props.price}</p>
+                <img src={this.props.image} height={100}/>
+                <button onClick={() => {
+                    addToCart(this.props.name)
+                    addToCartPrice(this.props.price)
+                }}>
+                    Add To Cart
+                </button>
+            </div>
+        );
+    }
+}
+
+export default BakeryItem
